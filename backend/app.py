0ffd7dc6c03603abcfefb25bfd68c0a81f27235b
t-iffany <tiffanyxleong@gmail.com>
2023-09-11 23:08:42 -0700

    Install flask_cors to handle CORS errors. Add app.route for POST requests to /capture-image.

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 # import Flask class from the flask module
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
+from flask_cors import CORS
 
 # create a Flask web application instance called 'app'
 app = Flask(__name__)
+CORS(app)
 
 # define a route for the flask server
 @app.route('/')
@@ -14,7 +16,12 @@ def index():
 def favicon():
     # specify the path to the favicon.ico file
     return send_from_directory(app.root_path, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-  
+
+# define a route to capture image
+@app.route('/capture-image', methods=['POST'])
+def capture_image():
+    return jsonify({'result': 'Image captured successfully'})
+
 # start the Flask development server
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
