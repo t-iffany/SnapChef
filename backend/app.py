c1421836b90bed9484a45b39d9d3274d8718ac7a
t-iffany <tiffanyxleong@gmail.com>
2023-09-11 22:17:50 -0700

    Fix Flask app, works now

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,20 @@
 # import Flask class from the flask module
-from flask import Flask, request, jsonify
+from flask import Flask, send_from_directory
 
 # create a Flask web application instance called 'app'
 app = Flask(__name__)
 
-# define a route for the /capture endpoint, configured to accept HTTP POST requests
-@app.route('/capture', methods=['POST'])
-def capture_image():
-    # Capture an image from the camera here
-    # You can use libraries like OpenCV for this purpose
+# define a route for the flask server
+@app.route('/')
+def index():
+    return 'Flask server successfully running'
 
-    # Access data sent in the POST request
-    # For example, if the client sent JSON data, you can access it like this:
-    #  data = request.get_json()
-
-    # Process the image using TensorFlow and return results as JSON
-    # Example:
-    # image = capture_image_from_camera()
-    # processed_data = process_image_with_tensorflow(image)
-
-    # return processed data as JSON
-    # return jsonify(processed_data)
+# define a route to serve the favicon.ico file
+@app.route('/favicon.ico')
+def favicon():
+    # specify the path to the favicon.ico file
+    return send_from_directory(app.root_path, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
   
-  # start the Flask development server
-  if __name__ == '__main__':
-    app.run(debug=True)
+# start the Flask development server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
