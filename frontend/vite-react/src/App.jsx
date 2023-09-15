69b49dddb87f38b9cf155fa86664a5205d8a9c8e
t-iffany <tiffanyxleong@gmail.com>
2023-09-15 15:10:22 -0700

    Removed unused code and components from vite App.jsx

--- frontend/vite-react/src/App.jsx
+++ frontend/vite-react/src/App.jsx
@@ -9,22 +9,13 @@ function App() {
   return (
     <>
       <h1>Welcome to SnapChef!</h1>
-      <div className="container">
+      <div>
         {/* <p>Click the button to open the camera</p> */}
         <Camera />
+      </div>
+      <div>
         <ImgUpload />
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }
