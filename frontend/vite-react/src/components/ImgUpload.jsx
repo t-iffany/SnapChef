7efe2683170f9a3938c3cdf37bd6936a164fdd59
t-iffany <tiffanyxleong@gmail.com>
2023-09-15 15:08:33 -0700

    Removed unused/commented out code

--- frontend/vite-react/src/components/ImgUpload.jsx
+++ frontend/vite-react/src/components/ImgUpload.jsx
@@ -27,51 +27,6 @@ function ImgUpload() {
       return;
     }
 
-    // try {
-    //   const formData = new FormData();
-    //   formData.append('image', state.imgAsFile);
-
-    //   // // Extract file extension from the selected file
-    //   // const fileExtension = state.imgAsFile.name.split('.').pop().toLowerCase();
-
-    //   // // Include the file extension when sending data
-    //   // const formData = new FormData();
-    //   // formData.append('image', state.imgAsFile);
-    //   // formData.append('extension', fileExtension);
-
-
-    //   // send the image to the backend for classification
-    //   const response = await axios.post('http://127.0.0.1:5000/classify-image', formData, {
-    //     headers: {
-    //       'Content-Type': 'multipart/form-data', // Set the content type to multipart/form-data
-    //     },
-    //   });
-
-    //   console.log("response: ", response);
-    //   // Check if the response contains the expected data
-    //   if (response.data && response.data.predicted_class && response.data.confidence) {
-    //     // handle the response from the backend
-    //     const { predicted_class, confidence } = response.data;
-
-    //     setState((prev) => ({
-    //       ...prev,
-    //       imgResult: `Predicted Class: ${predicted_class}, Confidence: ${confidence.toFixed(2)}`,
-    //     }));
-    //   } else {
-    //     console.error('Invalid response format from the backend:', response.data);
-    //   }
-    // } catch (error) {
-    //   console.error('Error uploading and classifying the image:', error);
-
-    //   // display error message from the response directly
-    //   setState((prev) => ({
-    //     ...prev,
-    //     imgResult: 'Error from the backend: ${response.data}',
-    //   }));
-    // }
-
-
-
     const reader = new FileReader();
     reader.readAsDataURL(state.imgAsFile);
 
@@ -101,57 +56,9 @@ function ImgUpload() {
           console.log('Error uploading and classifying the image: ', err);
         });
 
-      // reader.readAsDataURL(state.imgAsFile);
-
     };
 
-      // // Check if the response contains the expected data
-      // if (response.data && response.data.predicted_class && response.data.confidence) {
-      //   // handle the response from the backend
-      //   const { predicted_class, confidence } = response.data;
-
-      //   setState((prev) => ({
-      //     ...prev,
-      //     imgResult: `Predicted Class: ${predicted_class}, Confidence: ${confidence.toFixed(2)}`,
-      //   }));
-      //   } else {
-      //   console.error('Invalid response format from the backend:', response.data);
-      // }
-     
-
   };
-
-  // const reader = new FileReader();
-  // reader.readAsDataURL(state.imgAsFile);
-
-  // reader.onload = (e) => {
-  //   const base64Image = e.target.result;
-
-  //   axios
-  //       .post("http://127.0.0.1:5000/classify-image",
-  //       JSON.stringify(base64Image)
-  //       , {
-  //         headers: {
-  //           "Content-Type": "application/json; charset=UTF-8",
-  //         },
-  //       })
-  //       .then((res) => {
-  //         console.log('res', res);
-  //         setState(prev => ({ ...prev, imageResult: res.data }));
-  //         document.querySelector("input[type='file']").value = "";
-  //         state.submitted(true);
-  //       })
-  //       .catch((err) => {
-  //           console.log('Error uploading and classifying the image: ', err);
-  //       });
-
-  //     // reader.readAsDataURL(state.imgAsFile);
-
-  //   console.log('e.target.result: ', e.target.result);
-  //   console.log('imgAsFile: ', imgAsFile);
-  //   console.log('JSON.stringify(base64Image): ', JSON.stringify(base64Image))
-
-  // };
 
 
   return (
@@ -170,7 +77,6 @@ function ImgUpload() {
         )}
       </div>
 
-      {/* <form onSubmit={handleUpload}> */}
       <form>
         <input
           id="inputTag"
